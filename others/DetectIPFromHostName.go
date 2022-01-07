e889fc397239d359c238aa1beb5345f839f18af1
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-07 14:12:30 +0900

    その他

---
+++ others/DetectIPFromHostName.go
@@ -0,0 +1,15 @@
+package others
+
+import "net"
+
+/*
+GetHtmlFromHostname => arg:ipaddress string/return:[]string, error
+					=> IPアドレスから逆引きでホスト名を検索する関数
+*/
+func GetHostnameFromIPAddress(ipaddress string) ([]string, error) {
+	hostname, err := net.LookupAddr(ipaddress)
+	if err != nil {
+		return nil, err
+	}
+	return hostname, nil
+}
