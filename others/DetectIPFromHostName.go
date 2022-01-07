ee19c7963eaff515eed9afd78eb2d0d541d919fe
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-07 14:15:58 +0900

    IPアドレスから逆引きでホスト名を検索する関数

--- others/DetectIPFromHostName.go
+++ others/DetectIPFromHostName.go
@@ -6,6 +6,7 @@ import "net"
 GetHtmlFromHostname => arg:ipaddress string/return:[]string, error
 					=> IPアドレスから逆引きでホスト名を検索する関数
 */
+
 func GetHostnameFromIPAddress(ipaddress string) ([]string, error) {
 	hostname, err := net.LookupAddr(ipaddress)
 	if err != nil {
